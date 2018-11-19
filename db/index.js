1fa975456dddec8c58900f266847e133b8c101a9
Darren Lau <darrlau@gmail.com>
2018-11-19 13:17:34 -0800

    seed files

--- db/index.js
+++ db/index.js
@@ -1,6 +1,12 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
+const {DocumentStore} = require('ravendb');
 // const db = mongoose.connect('mongodb://localhost/FECreviews');
 // const db = mongoose.connect('mongodb://mongo:27017/FECreviews');
 // const db = mongoose.connect(process.env.MONGO_URI || 'mongodb://max:reviews7@ds151970.mlab.com:51970/reviews');
-const db = mongoose.connect("mongodb://max:reviews7@ds151970.mlab.com:51970/reviews");
-module.exports = db;
+// const db = mongoose.connect("mongodb://max:reviews7@ds151970.mlab.com:51970/reviews");
+
+
+const store = new DocumentStore('http://localhost:8080', 'test');
+store.initialize();
+
+module.exports = store;
--- db/seed.js
+++ db/seed.js
@@ -2,28 +2,64 @@ const Reviews = require('./Review.js');
 const db = require('./index.js');
 const faker = require('faker');
 
-for (let c = 1; c <= 100; c++) {
-  let rand = Math.floor(Math.random() * 20 + 20);
-  for (let i = 0; i < rand; i++) {
+const {DocumentStore} = require('ravendb');
+const store = new DocumentStore('http://localhost:8080', 'test');
+store.initialize();
+const session = store.openSession();
+const bulkInsert = store.bulkInsert();
 
-    let record = {};
-    let star = Math.floor(Math.random() * 5 + 1);
-    let helpful = Math.floor(Math.random() * 20) * (Math.random() < 0.1 ? -1 : 1);
-    let reported = Math.random() < 0.01 ? true : false;
-    
-    record.stars = star;
-    record.course = c;
-    record.body = faker.lorem.paragraph();
-    record.author = faker.name.findName();
-    record.authorImgUrl = faker.image.avatar();
-    record.created = faker.date.past();
-    record.wasHelpful = helpful;
-    record.reported = reported;
-    console.log(record.stars)
-    Reviews.create(record, (err, data)=>{
-      if (err) {
-        console.log(err, 'error popultaing the db');
-      }
-    });
+async function bulk() {
+  for (let c = 1; c <= 1000000; c++) {
+    let rand = Math.floor(Math.random() * 20 + 20);
+    for (let i = 0; i < rand; i++) {
+  
+      let record = {};
+      let star = Math.floor(Math.random() * 5 + 1);
+      let helpful = Math.floor(Math.random() * 20) * (Math.random() < 0.1 ? -1 : 1);
+      let reported = Math.random() < 0.01 ? true : false;
+      
+      record.stars = star;
+      record.course = c;
+      record.body = faker.lorem.paragraph();
+      record.author = faker.name.findName();
+      record.authorImgUrl = faker.image.avatar();
+      record.created = faker.date.past();
+      record.wasHelpful = helpful;
+      record.reported = reported;
+      record.id = 'test/';
+      record.documentId = 'test/';
+      
+      await bulkInsert.store(record)
+        // .then(() => bulkInsert.saveChanges())
+        // .catch((err) => {console.log(err, 'problem with DB')})
+    }
   }
+  await bulkInsert.finish();
 }
+
+bulk();
+
+// for (let c = 100; c <= 300; c++) {
+//   let rand = Math.floor(Math.random() * 20 + 20);
+//   for (let i = 0; i < rand; i++) {
+
+//     let record = {};
+//     let star = Math.floor(Math.random() * 5 + 1);
+//     let helpful = Math.floor(Math.random() * 20) * (Math.random() < 0.1 ? -1 : 1);
+//     let reported = Math.random() < 0.01 ? true : false;
+    
+//     record.stars = star;
+//     record.course = c;
+//     record.body = faker.lorem.paragraph();
+//     record.author = faker.name.findName();
+//     record.authorImgUrl = faker.image.avatar();
+//     record.created = faker.date.past();
+//     record.wasHelpful = helpful;
+//     record.reported = reported;
+    
+//     session.store(record)
+//       .then((user) => console.log(user))
+//       .then(() => session.saveChanges())
+//       .catch((err) => {console.log(err, 'problem with DB')})
+//   }
+// }
